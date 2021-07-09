82bb6b43dae6e1f81d513cb94dc0bc527eea9fed
Laurence Hoummadi <hoummadi@orange.fr>
2021-07-09 15:48:05 +0200

    rt

--- detailComand.php
+++ detailComand.php
@@ -4,30 +4,65 @@ include("include/header.php");
 
 //définition de variables
 $ID = $_GET["id"]; //récupère l'ID de la commande
-$textSQL=getById("commandes",$ID);//sous la variable textSQL on va chercher les commandes dans le fichier strings
-$RSCommandes=$pdo->query($textSQL);//sous la variable RScommande on se connecte à la BDD et on execute la variable au dessus
-$RowsCommandes=$RSCommandes->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-<table class="tableauCmdes">
-    <tr>
-        <th>Id Commande</th>
-        <th>Date commande</th>
-        <th>Client n°</th>
-    </tr>
-   
-    <?php 
-        foreach ($RowsCommandes as $ind=>$rowCommandes) {
-            //déclaration des variables
-            $commandeID=$rowCommandes["Commande_ID"];
-            $commandeDate=$rowCommandes["Commande_Date"];
-            $commandeClient=$rowCommandes["Commande_Client_ID"];
-        
-            //affichage  
-    ?>
-    <tr>
-        <td><?php echo $commandeID;?></td>
-        <td><?php echo $commandeDate;?></td>
-        <td><?php echo $commandeClient;?></td>
-    </tr>
-<?php }?>
+
+<div class = "container">
+    <div class = "row">
+        <div class="col-md-6">
+            <table class="tableauCmdes">
+                
+                    <?php $textSQL=getById("commandes",$ID);//sous la variable textSQL on va chercher les commandes dans le fichier strings
+                            $RSCommandes=$pdo->query($textSQL);//sous la variable RScommande on se connecte à la BDD et on execute la variable au dessus
+                            $RowsCommandes=$RSCommandes->fetchAll(PDO::FETCH_ASSOC);
+                    ?>
+                <thead>       
+                        <tr>
+                            <th>Id Commande</th>
+                            <th>Date commande</th>
+                            <th>Client n°</th>
+                        </tr>
+                </thead>
+
+                <tbody>
+                        <?php 
+                            foreach ($RowsCommandes as $ind=>$rowCommandes) {
+                                //déclaration des variables
+                                $commandeID=$rowCommandes["Commande_ID"];
+                                $commandeDate=$rowCommandes["Commande_Date"];
+                                $commandeClient=$rowCommandes["Commande_Client_ID"];
+                    
+                        //affichage  
+                        ?>
+                    <tr>
+                        <td><?php echo $commandeID;?></td>
+                        <td><?php echo $commandeDate;?></td>
+                        <td><?php echo $commandeClient;?></td>
+                    </tr>
+                        <?php }?>
+                </tbody>
+        </div>
+        <div class="col-md-6">
+            <div class="tableauProd">
+                <thead>
+                    <tr>
+                        <th>ID Produit</th>
+                        <th>Nom Produit</th>
+                        <th>Image Produit</th>
+                        <th>Prix Produit</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                </tbody>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
