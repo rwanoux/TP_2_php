07501c00fdf57ae262f89c0bdc247a812fecd29d
Laurence Hoummadi <hoummadi@orange.fr>
2021-07-09 10:32:29 +0200

    modif footer

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,15 @@
 </body>
+<footer>
+    <h3>Titre</h3>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores possimus sequi suscipit culpa provident,
+        sapiente nam. Laboriosam porro non fugit nam vitae necessitatibus ab possimus, amet repudiandae illo dicta
+        pariatur.
+    </p>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
 
 </html>
+>
