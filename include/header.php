b2872ca15d6c1579f089e5769fa344a267849912
Laurence Hoummadi <hoummadi@orange.fr>
2021-07-09 09:51:25 +0200

    creation header footer

---
+++ include/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    test
+                </li>
+
+                <li>
+                    test
+                </li>
+                <li>
+                    test
+                </li>
+                <li>
+                    test
+                </li>
+
+            </ul>
+
+        </nav>
+
+
+    </header>
