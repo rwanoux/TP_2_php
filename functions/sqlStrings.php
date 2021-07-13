7d5f75515748759816e0110a7dcf2b8c0a57e8a9
Erwan LEMAIRE <erwan_lemaire@yahoo.fr>
2021-07-13 10:47:04 +0200

    ozfdfn

--- functions/sqlStrings.php
+++ functions/sqlStrings.php
@@ -1,43 +1,42 @@
 <?php
 
 
-function getAllFrom($table){
+function getAllFrom($table)
+{
 
-return "SELECT * FROM ".$table;
+    return "SELECT * FROM " . $table;
 }
 
 
-function getById($table, $Id){
-    $sql;
-    switch ($table){
+function getById($table, $Id)
+{
+    switch ($table) {
         case "clients":
-            $sql= "SELECT * FROM clients WHERE Client_Id =".$Id;
+            $sql = "SELECT * FROM clients WHERE Client_Id =" . $Id;
             break;
-        
+
         case "cmd_pdt":
-            $sql= "SELECT * FROM cmd_pdt WHERE Cmd_pdt_ID =".$Id;
+            $sql = "SELECT * FROM cmd_pdt WHERE Cmd_pdt_ID =" . $Id;
 
             break;
         case "produits":
-            $sql= "SELECT * FROM produits WHERE Produit_ID =".$Id;
+            $sql = "SELECT * FROM produits WHERE Produit_ID =" . $Id;
 
             break;
 
-         case "commandes":
-            $sql= "SELECT * FROM commandes WHERE Commande_Id =".$Id;
-             break;
-
+        case "commandes":
+            $sql = "SELECT * FROM commandes WHERE Commande_Id =" . $Id;
+            break;
     }
     return $sql;
-
- 
 }
-function getCommandsByClientId($clientId){
-    return "SELECT * FROM commandes WHERE Commande_Client_ID ='".$clientId."'";
-} 
-function getProdFromCommandId($comId){
-//SELECT SUM(produits.Produit_Prix) AS totalTTC, SUM(produits.Produit_Prix*0.8) AS totalHT,
+function getCommandsByClientId($clientId)
+{
+    return "SELECT * FROM commandes WHERE Commande_Client_ID ='" . $clientId . "'";
+}
+function getProdFromCommandId($comId)
+{
+    //SELECT SUM(produits.Produit_Prix) AS totalTTC, SUM(produits.Produit_Prix*0.8) AS totalHT,
     return "SELECT produits.Produit_Nom, produits.Produit_Image, produits.Produit_Prix AS TTC, produits.Produit_Prix*0.8 AS HT 
-    FROM  cmd_pdt, produits WHERE produits.Produit_ID=cmd_pdt.Cmd_Pdt_Produit_ID AND cmd_pdt.Cmd_Pdt_Commande_ID=".$comId;
+    FROM  cmd_pdt, produits WHERE produits.Produit_ID=cmd_pdt.Cmd_Pdt_Produit_ID AND cmd_pdt.Cmd_Pdt_Commande_ID=" . $comId;
 }
-?>
